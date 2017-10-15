7e60b904a53066cb644f33cc7871fa4125bf4319
Pierros Papadeas <pierros@papadeas.gr>
2017-10-15 10:36:08 +0100

    Cosmetic changes on stations page

--- network/static/js/stations.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    'use strict';
-
-    // Render Station success rate
-    var success_rate = $('.gs.progress-bar-success').data('success-rate');
-    var percentagerest = $('.gs.progress-bar-danger').data('percentagerest');
-    $('.gs.progress-bar-success').css('width', success_rate + '%');
-    $('.gs.progress-bar-danger').css('width', percentagerest + '%');
-});
